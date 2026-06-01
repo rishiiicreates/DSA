c8f34d273049b4076a4a70eac42682137b03f330
Hrishikesh Yadav <rishiicreates@gmail.com>
2026-06-01 08:35:06 +0530

    Longest Repeating subseqence added

---
+++ Dynamic Programming/Longest common subsequence/problems/longest_repeating_subseq.cpp
@@ -0,0 +1,34 @@
+//problem statement -> logic -> code variation
+//input: arr[string]
+//task: find longest repeating subsequence (order matters; can be discontinuous) that repeat itself
+//ex: "aabebcdd" -> "abd" and "abd" we can find two of these and repeat itself ( find the longest one)
+//output: print the length of largest subsequence
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lrs(const string& s){
+    int len = s.size();
+    string k = s;
+
+    vector<vector<int>> t(len+1, vector<int>(len+1, 0));
+
+    for(int i = 1; i < len+1; i++){
+        for(int j = 1; j < len+1; j++){
+            if(s[i-1]==k[j-1] && i != j){
+                t[i][j] = t[i-1][j-1] + 1;
+            }else{
+                t[i][j] = max(t[i-1][j], t[i][j-1]);
+            }
+        }
+    }
+
+    return t[len][len];
+}
+
+int main(){
+    string s = "aabebcdd";
+    cout<<lrs(s);
+    return 0;
+}
